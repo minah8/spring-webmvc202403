d2751429e106ac0fb98d38875059a32520a2c0ab
minah8 <o_uo_o@naver.com>
2024-04-08 15:48:34 +0900

    #40. 공공데이터 API 사용 방법(기상청 단기예보)

--- src/main/java/com/spring/mvc/config/LocalResourceConfig.java
+++ src/main/java/com/spring/mvc/config/LocalResourceConfig.java
@@ -1,11 +1,13 @@
 package com.spring.mvc.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // 브라우저에서는 직접 로컬 경로를 지목해서 데이터를 가져오지 못합니다.
 // 로컬에 저장된 이미지 경로를 웹 브라우저에서 불러올 수 있게 URL을 만드는 설정
+@Configuration
 public class LocalResourceConfig implements WebMvcConfigurer {
 
     @Value("${file.upload.root-path}")
